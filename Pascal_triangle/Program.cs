b9266b7f45a15ae14c5027c5cc85aaf169126620
Danya <theincredibledanzel@yandex.ru>
2022-07-08 19:57:20 +0400

    edit

---
+++ Pascal_triangle/Program.cs
@@ -0,0 +1,90 @@
+﻿
+// Вариант из семинара
+// Корректно отображает первые 5 строк
+int y = 5;
+int x = 2 * y - 1;
+int [,] triangle = new int[y, x];
+FillTriangle(triangle);
+Console.WriteLine();
+PrintTriangle(triangle);
+Console.WriteLine();
+
+
+void FillTriangle(int [,] a)
+{
+    int middle = y - 1;
+    int left = middle;
+    int right = middle;
+    for(int i = 0; i < y; i++)
+    {
+        for(int j = 0; j < x; j++)
+        {
+            if(left == j && right == j)
+            {
+                a[i,j] = 1;
+                left--;
+                right++;
+                break;
+            }
+            else
+            {
+                if(left == j)
+                {
+                    a[i,j] = 1;
+                    left--;
+                }
+                else
+                {
+                    if(right == j)
+                    {
+                        a[i,j] = 1;
+                        right++;
+                        break;
+                    }
+                    else
+                    {
+                        a[i,j] = 0;
+                    }
+                }
+            }
+        }
+    }
+    for(int i = 2; i < y; i++)
+    {
+        for(int j = 0; j < x; j++)
+        {
+            if(j != 0 && j < x - 1 && a[i,j] == 0)
+            {
+                a[i,j] = a[i - 1, j - 1] + a[i - 1, j + 1];
+            }
+        }
+    }
+}
+void PrintTriangle(int [,] a)
+{
+    for(int i = 0; i < a.GetLength(0); i++)
+    {
+        for(int j = 0; j < a.GetLength(1); j++)
+        {
+            if(a[i,j] != 0)
+            {
+                if(a[i,j] < 10)
+                {
+                    Console.Write($"{a[i,j]}  ");
+                }
+                else
+                {
+                    if(a[i,j] < 100)
+                    {
+                        Console.WriteLine($"{a[i,j]} ");
+                    }
+                }
+            }
+            else
+            {
+                Console.Write("    ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
